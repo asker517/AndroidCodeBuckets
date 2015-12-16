df5ec428634d97bd29118d95d7281ddd0bd117d7
Vincent <6781219@qq.com>
2015-12-16 15:40:09 +0800

    exclude ignore file

---
+++ AndroidRetrofit/app/src/main/java/me/onez/androidretrofit/ApiService.java
@@ -0,0 +1,7 @@
+package me.onez.androidretrofit;
+
+/**
+ * Created by Vincent on 15/12/16.
+ */
+public interface ApiService {
+}
---
+++ AndroidRetrofit/app/src/main/java/me/onez/androidretrofit/model/CityInfo.java
@@ -0,0 +1,7 @@
+package me.onez.androidretrofit.model;
+
+/**
+ * Created by Vincent on 15/12/16.
+ */
+public class CityInfo {
+}
