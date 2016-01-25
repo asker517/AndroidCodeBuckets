b71250070864cba2bc996575f0c423e09f40a7ee
Vincent <6781219@qq.com>
2016-01-25 09:57:27 +0800

    add a simple Rx OnError handle test.

--- AndroidRx/app/src/main/java/me/onez/rxinandroid/MainActivity.java
+++ AndroidRx/app/src/main/java/me/onez/rxinandroid/MainActivity.java
@@ -28,8 +28,9 @@ public class MainActivity extends BaseRxActivity {
       @Override
       public void onClick(View v) {
         //useFlatMap();
-        //        useConcatMap();
-        doOnNextThreadTest();
+        //useConcatMap();
+        //doOnNextThreadTest();
+        //onErrorHandleTest();
       }
     });
   }
@@ -105,7 +106,7 @@ public class MainActivity extends BaseRxActivity {
       }
     });
 
-    Observable.from(new String[]{"1", "2", "0"}).map(new Func1<String, Integer>() {
+    Observable.from(new String[] { "1", "2", "0" }).map(new Func1<String, Integer>() {
       @Override
       public Integer call(String s) {
         return Integer.parseInt(s) + 10086;
@@ -245,28 +246,34 @@ public class MainActivity extends BaseRxActivity {
     });
   }
 
-	/**
-	 * doOnNext测试
-	 */
-	private void doOnNextThreadTest() {
+  /**
+   * doOnNext线程测试: call0和call1会受到subscribeOn的线程影响,
+   * call2受到observeOn的线程影响,切换到新的线程,
+   * call3继续利用observeOn变幻线程,切换到主线程.
+   */
+  private void doOnNextThreadTest() {
     observable().doOnNext(new Action1<String>() {
       @Override
       public void call(String s) {
-        Log.d(TAG, "call0: "+Thread.currentThread().getName());
+        //RxCachedThreadScheduler-2
+        Log.d(TAG, "call0: " + Thread.currentThread().getName());
       }
     }).subscribeOn(Schedulers.io()).doOnNext(new Action1<String>() {
       @Override
       public void call(String s) {
+        //RxCachedThreadScheduler-2
         Log.d(TAG, "call1: " + Thread.currentThread().getName());
       }
     }).observeOn(Schedulers.io()).doOnNext(new Action1<String>() {
       @Override
       public void call(String s) {
+        //RxCachedThreadScheduler-1
         Log.d(TAG, "call2: " + Thread.currentThread().getName());
       }
     }).observeOn(AndroidSchedulers.mainThread()).doOnNext(new Action1<String>() {
       @Override
       public void call(String s) {
+        //main
         Log.d(TAG, "call3: " + Thread.currentThread().getName());
       }
     }).subscribe(new Observer<String>() {
@@ -282,7 +289,62 @@ public class MainActivity extends BaseRxActivity {
 
       @Override
       public void onNext(String s) {
+        //main
         Log.d(TAG, "call4: " + Thread.currentThread().getName());
+      }
+    });
+  }
+
+  /**
+   * 当调用onErrorReturn时,当error发生,onErrorReturn会接受到异常,
+   * 可以在这了定义预发射的数据继续发射
+   */
+  private void onErrorHandleTest() {
+    Observable.create(new Observable.OnSubscribe<Integer>() {
+      @Override
+      public void call(Subscriber<? super Integer> subscriber) {
+        for (int i = 0; i < 10; i++) {
+          if (i < 5) {
+            subscriber.onNext(i);
+          } else {
+            //当数据大于5时,触发错误,但是onErrorReturn只会触发一次
+            Log.d(TAG, "on Error call: ");//这里会调用多次
+            subscriber.onError(new Throwable("Error Happens"));
+          }
+        }
+      }
+    }).onErrorReturn(new Func1<Throwable, Integer>() {
+      @Override
+      public Integer call(Throwable throwable) {
+        //当onError后,触发onErrorReturn,这里可以预先定义发射数据,eg.发射元素 10
+        //然后正常终止,只会首次触发
+        Log.d(TAG, "onErrorReturn call: ");
+        return 10;
+      }
+    }).filter(new Func1<Integer, Boolean>() {
+      @Override
+      public Boolean call(Integer integer) {
+        if (integer.intValue() > 9) {
+          //onErrorReturn发射的数据接下来的操作符会正常处理
+          Log.d(TAG, "filter call: " + integer.intValue());
+          return false;
+        }
+        return true;
+      }
+    }).subscribe(new Observer<Integer>() {
+      @Override
+      public void onCompleted() {
+        Log.d(TAG, "onCompleted: ");
+      }
+
+      @Override
+      public void onError(Throwable e) {
+        Log.d(TAG, "onError: " + e.getMessage());
+      }
+
+      @Override
+      public void onNext(Integer integer) {
+        Log.d(TAG, "onNext: " + integer.intValue());
       }
     });
   }
