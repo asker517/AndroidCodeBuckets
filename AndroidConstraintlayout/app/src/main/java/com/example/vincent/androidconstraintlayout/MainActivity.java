7a26be1fba41c972c98d72d00c572744e0e7c586
Vincent <Vincent@Vincents-MBP.local>
2017-08-29 16:23:16 +0800

    add constraintlayout practice

---
+++ AndroidConstraintlayout/app/src/main/java/com/example/vincent/androidconstraintlayout/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.vincent.androidconstraintlayout;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * <P>Created by Vincent on 2017/8/29.</P>
+ */
+public class MainActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(@Nullable Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+  }
+}
