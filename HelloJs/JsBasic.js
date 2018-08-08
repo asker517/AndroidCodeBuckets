5143c6a5df341eee11dd8b09d59b4e490cc030f9
Vincent Zuo <asker.zuo@gmail.com>
2018-08-08 16:12:11 +0800

    add js practice

---
+++ HelloJs/JsBasic.js
@@ -0,0 +1,34 @@
+var x = 10; //Number
+var name = "vincent"; //String
+var arrs = ["a", "b"]; //Array
+var person = { name: "zuo", age: 20 }; //Object
+
+
+var y = 5;
+var y;
+console.log(y) // 5
+
+var z; //undefined
+var z = 5; //Number
+var z = "hhh"; // String
+
+
+//创建对象
+var car = {
+    model: "BMW",
+    color: "Black",
+    price: "23W"
+};
+
+//对象属性调用方式
+car.model;
+car["color"];
+
+//JS函数
+function myJsFunction(params1,params2){
+    console.log("invoke this function log");
+}
+
+
+
+
